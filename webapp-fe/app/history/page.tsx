2ddea0ebc9405551f61541045ec2d72bec1ec11f
choguun <visaruth.s@gmail.com>
2023-11-05 22:25:14 +0700

    update

--- webapp-fe/app/history/page.tsx
+++ webapp-fe/app/history/page.tsx
@@ -1,11 +1,42 @@
 'use client';
 
-import React from 'react'
-import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { NavBar } from '@/components/NavBar';
+import { getAccount } from '@/lib/service';
 
 const HistoryPage = () => {
     const router = useRouter();
+
+    const [account, setAccount] = useState<any>({});
+    const [savingWalletAddress, setSavingWalletAddress] = useState<string>('');
+    const [cryptoWalletAddress, setCryptoWalletAddress] = useState<string>('');
+
+
+    useEffect(() => {
+        const getAccountData = async () => {
+            try {
+                const data = await getAccount();    
+
+               let account = {
+                    crypto_wallet_address :  data.crypto_wallet_address,
+                    saving_wallet_address : data.saving_wallet_address,
+                    crypto_wallet_salt : data.crypto_wallet_salt,
+                    saving_wallet_salt : data.saving_wallet_salt,
+                };
+                
+                setAccount(account);
+                setSavingWalletAddress(data.saving_wallet_address);
+                setCryptoWalletAddress(data.saving_wallet_address);
+            } catch(error : any) {
+                console.error(error);
+                router.push('/login');
+            }
+        }
+        // if(token.length > 0) {
+          getAccountData();
+        // }
+      }, []);
 
     return (
         <div className="bg-gray-200 w-[600px]">
@@ -14,9 +45,9 @@ const HistoryPage = () => {
             </div>
             <div className="p-6">
                 <span className="text-xl font-semibold">Saving Account Wallet</span><br/>
-                <span className="underline mb-5"><a href="https://mumbai.polygonscan.com/address/0xC89382FDDADebde2a28c17ab94BE5Cc04eF07766" target="_blank">Explorer</a></span><br/><br/>
+                <span className="underline mb-5"><a href={`https://mumbai.polygonscan.com/address/${savingWalletAddress}`} target="_blank">Explorer</a></span><br/><br/>
                 <span className="text-xl font-semibold mt-5">Crypto Wallet</span><br/>
-                <span className="underline"><a href="https://mumbai.polygonscan.com/address/0x0Cb0eb853B521e94Be0d9D18A1B4C432fee13aD3" target="_blank">Explorer</a></span>
+                <span className="underline"><a href={`https://mumbai.polygonscan.com/address/${cryptoWalletAddress}`} target="_blank">Explorer</a></span>
             </div>
             {/* Bottom Navbar */}
             <NavBar path={'/history'} />
